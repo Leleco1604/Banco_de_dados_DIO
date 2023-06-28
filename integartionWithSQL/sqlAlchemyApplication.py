d6a5044026bb259586e85bd99ccd2d6c23c631c3
Leleco1604 <leonardogbarbosamelo2003@gmail.com>
2023-06-28 12:29:28 -0300

    changes

--- integartionWithSQL/sqlAlchemyApplication.py
+++ integartionWithSQL/sqlAlchemyApplication.py
@@ -1,6 +1,8 @@
 import sqlalchemy
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+from sqlalchemy import inspect
 from sqlalchemy import Column 
 from sqlalchemy import Integer
 from sqlalchemy import String
@@ -49,6 +51,28 @@ class Address(Base):
 
 print(User.__tablename__)
 print(Address.__tablename__)
+
+
+#conexão com o banco de dados
+engine = create_engine("sqlite://")
+
+
+#Criando as Classes com tabelas no banco de dados 
+Base.metadata.create_all(engine)
+
+#depreciado - será removido em um futuro release 
+inspetor_engine = inspect(engine)
+
+print(inspetor_engine.has_table("user_account"))
+
+#get_table_names seria um metodo para puxar o das tabelas que no caso seria address e user 
+print(inspetor_engine.get_table_names())
+
+
+
+
+
+
 
 
 
